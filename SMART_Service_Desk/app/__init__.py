86d1bdfec4de4083796c213bc14c85eb1bf6f84d
shammas012 <shammas012@gmail.com>
2025-04-22 16:38:23 +0100

    flask app up and running with hardcoded message after initialization.

---
+++ SMART_Service_Desk/app/__init__.py
@@ -0,0 +1,37 @@
+#references
+#https://flask.palletsprojects.com/en/latest/patterns/appfactories/
+#https://flask-sqlalchemy.palletsprojects.com/en/latest/quickstart/#configuration
+#
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+dbEngine = SQLAlchemy()
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     app.config.from_pyfile(os.path.join(os.path.dirname(__file__), 'config.py'))
+
+#     print("Config file loaded:", app.config.get('SQLALCHEMY_DATABASE_URI'))
+
+#     print("SQLALCHEMY_DATABASE_URI:", app.config.get('SQLALCHEMY_DATABASE_URI'))
+ 
+#     dbEngine.init_app(app)
+
+#     return app
+
+
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    print("SQLALCHEMY_DATABASE_URI:", app.config.get('SQLALCHEMY_DATABASE_URI'))
+    dbEngine.init_app(app)
+    @app.route('/')
+    def index():
+        return "Smart SD is live!"
+    return app
