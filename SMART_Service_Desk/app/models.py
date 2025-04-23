d4b1f1002c3357ce52363abedee7aa0f2ba31806
shammas012 <shammas012@gmail.com>
2025-04-23 19:50:50 +0100

    models completed for phase 1

--- SMART_Service_Desk/app/models.py
+++ SMART_Service_Desk/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from . import db
 import uuid as guid
 
@@ -16,35 +17,59 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
 
+class Project(db.Model):
+    __tablename__ = "projects"
+    id = db.Column(db.String(100), primary_key=True, default=generateGuid)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    shortName = db.Column(db.String(10), unique=True, nullable=False)
+    createdBy = db.Column(db.String(100), db.ForeignKey('users.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    tickets = db.relationship('Ticket', backref='project')
+
 class IssueType(db.Model):
     __tablename__ = "issue_types"
     id = db.Column(db.String(100), primary_key = True, default = generateGuid)
     description = db.Column(db.String(100), unique=True)
 
-class Ticket(db.Model):
-    __tablename__ = "tickets"
-    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
-    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
-
-class TicketHistory(db.Model):
-    __tablename__ = "ticket_history"
-    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
-    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
-
 class WorkflowStatus(db.Model):
-    __tablename__ = "user_roles"
+    __tablename__ = "workflow_statuses"
     id = db.Column(db.String(100), primary_key = True, default = generateGuid)
     description = db.Column(db.String(100), unique=True)
+    is_terminal = db.Column(db.Boolean, default=False)
+    is_default = db.Column(db.Boolean, default=False)
+
 
 class WorkflowTransition(db.Model):
     __tablename__ = "workflow_transitions"
     id = db.Column(db.String(100), primary_key = True, default = generateGuid)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
-    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
+    fromStatus = db.Column(db.String(100), db.ForeignKey('workflow_statuses.id'))
+    toStatus = db.Column(db.String(100), db.ForeignKey('workflow_statuses.id'))
+
+
+class Ticket(db.Model):
+    __tablename__ = "tickets"
+    key = db.Column(db.String(100), primary_key = True)
+    summary = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    createdBy = db.Column(db.String(100), db.ForeignKey('users.id'))
+    assignedTo = db.Column(db.String(100), db.ForeignKey('users.id'))
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+    updatedAt = db.Column(db.DateTime, default=datetime.utcnow)
+    issueTypeId = db.Column(db.String(100), db.ForeignKey('issue_types.id'))
+    statusId = db.Column(db.String(100), db.ForeignKey('workflow_statuses.id'))
+    issueType = db.relationship('IssueType', backref='tickets')
+    projectId = db.Column(db.String(100), db.ForeignKey('projects.id'))
+
+class TicketHistory(db.Model):
+    __tablename__ = "ticket_history"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    ticketKey = db.Column(db.Integer, db.ForeignKey('tickets.key'))
+    fromStatus = db.Column(db.String(100))
+    toStatus = db.Column(db.String(100))
+    changed_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
 
 
