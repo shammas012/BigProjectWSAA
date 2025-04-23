b478fbbc32dafcaea9596eaf987857a7a1deeb40
shammas012 <shammas012@gmail.com>
2025-04-23 19:01:11 +0100

    Ticket, workflow, user related models added

--- SMART_Service_Desk/app/models.py
+++ SMART_Service_Desk/app/models.py
@@ -6,12 +6,43 @@ def generateGuid():
 
 class UserRole(db.Model):
     __tablename__ = "user_roles"
-    id = db.Column(db.String(100), primary_key = True, default = generateGuid())
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
     description = db.Column(db.String(100), unique=True)
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.String(100), primary_key = True, default = generateGuid())
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
+
+class IssueType(db.Model):
+    __tablename__ = "issue_types"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    description = db.Column(db.String(100), unique=True)
+
+class Ticket(db.Model):
+    __tablename__ = "tickets"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
+
+class TicketHistory(db.Model):
+    __tablename__ = "ticket_history"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
+
+class WorkflowStatus(db.Model):
+    __tablename__ = "user_roles"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
+    description = db.Column(db.String(100), unique=True)
+
+class WorkflowTransition(db.Model):
+    __tablename__ = "workflow_transitions"
+    id = db.Column(db.String(100), primary_key = True, default = generateGuid)
     username = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     roleid = db.Column(db.String(100), db.ForeignKey('user_roles.id'))
