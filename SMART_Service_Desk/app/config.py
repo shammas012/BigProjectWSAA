9c763910e926b9f7bff6f718449c04ec0f3e8081
shammas012 <shammas012@gmail.com>
2025-05-05 18:13:40 +0100

    auth.py added, password field added to user model.

--- SMART_Service_Desk/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'mysql://root:Aturoot@localhost:3306/smart_ticket_db'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
